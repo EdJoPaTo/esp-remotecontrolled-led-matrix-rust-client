a85ccfbafc90ef445ac5dd287f47fb9189621f65
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-05-30 16:50:28 +0200

    feat(snake): add snake example

---
+++ examples/snake.rs
@@ -0,0 +1,143 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+use esp_wlan_led_matrix_client::sync::Client;
+use rand::Rng;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl Point {
+    fn new(x: u8, y: u8) -> Self {
+        Self { x, y }
+    }
+
+    fn random(width: u8, height: u8) -> Self {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0..width - 1);
+        let y = rng.gen_range(0..height - 1);
+        Self { x, y }
+    }
+}
+
+struct Gamestate {
+    snake: Vec<Point>,
+    food: Point,
+    color: (u8, u8, u8),
+}
+
+impl Gamestate {
+    fn new(width: u8, height: u8) -> Self {
+        let food = Point::random(width, height);
+
+        let start = Point::new(width / 2, height / 2);
+        let end = {
+            let x = if start.x < food.x {
+                start.x - 1
+            } else {
+                start.x + 1
+            };
+            Point::new(x, start.y)
+        };
+
+        let r = rand::random::<u8>();
+        let g = rand::random::<u8>();
+        let b = rand::random::<u8>();
+
+        Self {
+            snake: vec![start, end],
+            food,
+            color: (r, g, b),
+        }
+    }
+}
+
+fn main() {
+    let addr = "espPixelmatrix:1337";
+
+    loop {
+        match Client::connect(addr) {
+            Ok(mut client) => {
+                println!(
+                    "size {}x{} = {} pixels",
+                    client.width(),
+                    client.height(),
+                    client.total_pixels()
+                );
+
+                if let Err(err) = snake(&mut client) {
+                    eprintln!("ERROR: {}", err);
+                }
+            }
+            Err(err) => {
+                eprintln!("CONNECT ERROR: {}", err);
+                sleep(Duration::from_millis(500));
+            }
+        }
+    }
+}
+
+fn snake(client: &mut Client) -> std::io::Result<()> {
+    client.fill(0, 0, 0)?;
+    let width = client.width();
+    let height = client.height();
+    let mut state = Gamestate::new(width, height);
+
+    loop {
+        let next_point = {
+            let start = &state.snake[0];
+            if start.x > state.food.x {
+                Point::new(start.x - 1, start.y)
+            } else if start.x < state.food.x {
+                Point::new(start.x + 1, start.y)
+            } else if start.y > state.food.y {
+                Point::new(start.x, start.y - 1)
+            } else {
+                Point::new(start.x, start.y + 1)
+            }
+        };
+
+        #[cfg(debug_assertions)]
+        println!(
+            "snake goes to {:3} {:3}  food is at {:3} {:3}",
+            next_point.x, next_point.y, state.food.x, state.food.y
+        );
+
+        if state.snake.contains(&next_point) {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("snake hit itself at length {}", state.snake.len()),
+            ));
+        }
+
+        if next_point == state.food {
+            state.food = Point::random(width, height);
+        } else {
+            let last = state.snake.pop().unwrap();
+            client.pixel(last.x, last.y, 0, 0, 0)?;
+        }
+
+        client.pixel(
+            next_point.x,
+            next_point.y,
+            state.color.0,
+            state.color.1,
+            state.color.2,
+        )?;
+        state.snake.insert(0, next_point);
+
+        client.pixel(
+            state.food.x,
+            state.food.y,
+            255 - state.color.0,
+            255 - state.color.1,
+            255 - state.color.2,
+        )?;
+
+        client.flush()?;
+        sleep(Duration::from_millis(200));
+    }
+}
