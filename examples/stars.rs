a2b9101d5d026ed8d6f2179bb5ae66c1aa2f35a7
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-05-10 17:28:49 +0200

    refactor(stars): use single connection

--- examples/stars.rs
+++ examples/stars.rs
@@ -5,59 +5,53 @@ use rand::Rng;
 use tokio::task;
 use tokio::time::sleep;
 
-#[allow(clippy::semicolon_if_nothing_returned)] // false positive
 #[tokio::main]
 async fn main() {
     let addr = std::env::var("ADDR");
     let addr = addr.as_deref().unwrap_or("espPixelmatrix:1337");
-    let mut client = Client::connect(addr).await.expect("failed to connect");
+    let client = Client::connect(addr).await.expect("failed to connect");
 
-    {
-        let mut client = client.clone();
-        task::spawn(async move {
-            loop {
-                if let Err(err) = stars(&mut client).await {
-                    println!("pixelflut_stars ERROR {}", err);
-                }
-                sleep(Duration::from_secs(5)).await;
+    loop {
+        let dur = match spawn_star(client.clone()).await {
+            Ok(_) => {
+                let dur = rand::thread_rng().gen_range(0..40_u64);
+                let dur = dur.pow(2);
+                Duration::from_millis(dur)
             }
-        });
+            Err(err) => {
+                eprintln!("spawn_star ERROR {}", err);
+                Duration::from_secs(2)
+            }
+        };
+        sleep(dur).await;
     }
-
-    let handle = {
-        task::spawn(async move {
-            loop {
-                if let Err(err) = stars(&mut client).await {
-                    println!("pixelflut_stars ERROR {}", err);
-                }
-                sleep(Duration::from_secs(5)).await;
-            }
-        })
-    };
-
-    // wait for the task to end which runds in an endless loop
-    handle.await.unwrap();
 }
 
-async fn stars(client: &mut Client) -> std::io::Result<()> {
-    loop {
-        let (dur, x, y) = {
-            let mut rng = rand::thread_rng();
-            let dur = rng.gen_range(0..800);
-            let x = rng.gen_range(0..client.width());
-            let y = rng.gen_range(0..client.height());
-            (dur, x, y)
-        };
-
-        println!("star {:3} {:3} followed by {:4} ms sleep", x, y, dur);
-
-        for bri in &[255_u8, 100, 0] {
-            let bri = bri.to_owned();
+async fn spawn_star(mut client: Client) -> std::io::Result<()> {
+    async fn fade_away(mut client: Client, x: u8, y: u8) -> std::io::Result<()> {
+        for bri in [100_u8, 0] {
+            sleep(Duration::from_millis(150)).await;
             client.pixel(x, y, bri, bri, bri).await?;
             client.flush().await?;
-            sleep(Duration::from_millis(150)).await;
         }
-
-        sleep(Duration::from_millis(dur)).await;
+        Ok(())
     }
+
+    let (x, y) = {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0..client.width());
+        let y = rng.gen_range(0..client.height());
+        (x, y)
+    };
+    println!("star {:3} {:3}", x, y);
+
+    client.pixel(x, y, 255, 255, 255).await?;
+    client.flush().await?;
+
+    task::spawn(async move {
+        if let Err(err) = fade_away(client, x, y).await {
+            println!("spawn_star ERROR {}", err);
+        }
+    });
+    Ok(())
 }
