6f9128dca6d6c3f48eaf5e25c808bb1ea51f1e40
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-04-22 19:08:45 +0200

    build(cargo): remove empty default feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod async_tokio;
 #[cfg(feature = "sync")]
 pub mod sync;
 
+#[cfg(any(feature = "sync", feature = "tokio"))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[non_exhaustive]
 #[repr(u8)]
